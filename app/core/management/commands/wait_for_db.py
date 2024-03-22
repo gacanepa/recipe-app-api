737a44a260814b4ffdedd65252079c65477c3326
Gabriel Cánepa <gacanepa@gmail.com>
2024-03-22 18:01:12 -0300

    Create skeleton for Django command

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,13 @@
+"""
+Django command to wait for the database to be available
+"""
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to pause execution until database is available"""
+
+    def handle(self, *args, **options):
+        self.stdout.write('Waiting for database...')
+        self.stdout.write('Database available!')
