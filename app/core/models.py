3b52fa948846b2c9d96c3ed0f4923d7569ef76ae
Gabriel Cánepa <gacanepa@gmail.com>
2024-05-27 12:04:32 -0300

    Address flake8 observations

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
 
 class UserManager(BaseUserManager):
 
@@ -19,7 +24,7 @@ class UserManager(BaseUserManager):
         user.is_superuser = True
         user.save(using=self._db)
         return user
-    
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
