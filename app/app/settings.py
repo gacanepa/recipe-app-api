9f043d0f5977ff148c72e3d588c54659bf1233e1
Gabriel Cánepa <gacanepa@gmail.com>
2024-05-27 11:51:43 -0300

    Set DEBUG conditionally

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yom3+i4f_7aw$vszhak@l5ts)hpddyik%s8nil9at5_@6r@_zt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = []
 
