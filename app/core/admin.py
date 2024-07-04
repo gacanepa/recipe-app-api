5664e1df4df804b57c388d336bf3e10daf3505e8
Gabriel Cánepa <gacanepa@gmail.com>
2024-07-04 11:46:52 -0300

    Register the custom user model in the Django admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,39 @@
-from django.contrib import admin # noqa
+"""
+Django admin customization
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    # Specifies the default ordering of users in the admin list view
+    ordering = ['id']
+    # Defines which fields of the user model are displayed in the list view,
+    list_display = ['email', 'name']
+    # Each inner tuple represents a section (which consists of an optional title and a dictionary of options)
+    fieldsets = (
+        # First section: None means no title
+        (None, {'fields': ('email', 'password')}),
+        # Second section: Personal Info title
+        ('Personal Info', {'fields': ('name',)}),
+        # Third section: Permissions title
+        (
+            'Permissions',
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        # Fourth section: Important dates
+        ('Important dates', {'fields': ('last_login',)})
+    )
+    # The add_fieldsets attribute is the same as fieldsets, but for the add page
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password', 'password_confirmation')
+        }),
+    )
+
+# Register the User model with the custom UserAdmin to the admin site
+admin.site.register(models.User, UserAdmin)
